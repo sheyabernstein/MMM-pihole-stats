14468b133b81270e564fb1ba15c46f63f0cf8c2d
Sheya Bernstein <sheyaberns@gmail.com>
2017-01-25 00:30:22 +0000

    check for top_sources length too

--- MMM-pihole-stats.js
+++ MMM-pihole-stats.js
@@ -49,7 +49,7 @@ Module.register('MMM-pihole-stats', {
 		header.innerHTML = this.ads_blocked_today + ' ads blocked today. (' + this.ads_percentage_today + '%)'
 		wrapper.appendChild(header);
 
-		if (this.top_sources && this.top_sources.length) {
+		if (this.top_sources && Object.keys(this.top_sources).length) {
 			var table = document.createElement('table');
 			table.className = 'xsmall light';
 			wrapper.appendChild(table);
@@ -73,11 +73,11 @@ Module.register('MMM-pihole-stats', {
 
 			for (var source in this.top_sources) {
 				var adCount = this.top_sources[source];
-
+				
 				if (this.config.showSourceHostnameOnly) {
 					var rx = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/
 					var ip = source.substring(source.lastIndexOf('(') + 1, source.lastIndexOf(')'));
-
+					
 					if (rx.test(ip)) {
 						hostname = source.substring(0, source.lastIndexOf('('))
 						if (hostname.length) {
@@ -187,8 +187,6 @@ Module.register('MMM-pihole-stats', {
 		}
 
 		this.top_sources = data['top_sources'] || [];
-		this.loaded = true;
-		this.updateDom(this.config.animationSpeed);
 	},
 
 })
