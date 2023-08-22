6a235850d74013d26a197b489468bddd997b1854
Sheya Bernstein <sheyaberns@gmail.com>
2023-08-23 00:57:01 +0100

    Add format code & stale workflows

--- MMM-pihole-stats.js
+++ MMM-pihole-stats.js
@@ -5,170 +5,176 @@
  * MIT Licensed.
  */
 
-Module.register('MMM-pihole-stats', {
-	// Default module config.
-	defaults: {
-		apiKey: '',
-		apiURL: 'http://pi.hole/admin/api.php',
-		port: 80,
-		showSources: true,
-		sourcesCount: 10,
-		showSourceHostnameOnly: true,
+Module.register("MMM-pihole-stats", {
+    // Default module config.
+    defaults: {
+        apiKey: "",
+        apiURL: "http://pi.hole/admin/api.php",
+        port: 80,
+        showSources: true,
+        sourcesCount: 10,
+        showSourceHostnameOnly: true,
 
-		updateInterval: 10 * 60 * 1000, // every 10 minutes
-		animationSpeed: 1000,
+        updateInterval: 10 * 60 * 1000, // every 10 minutes
+        animationSpeed: 1000,
 
-		floatingPoints: 2,
+        floatingPoints: 2,
 
-		retryDelay: 1000 * 30,
-		initialLoadDelay: 0,
-	},
+        retryDelay: 1000 * 30,
+        initialLoadDelay: 0,
+    },
 
-	formatInt: function (n) {
-		return n.toLocaleString();
-	},
+    formatInt: function (n) {
+        return n.toLocaleString();
+    },
 
-	formatFloat: function (n) {
-		if (this.config.floatingPoints) {
-			let x = 10 ** this.config.floatingPoints;
-			return Math.round(parseFloat(n) * x) / x;
-		} else {
-			return n;
-		}
-	},
+    formatFloat: function (n) {
+        if (this.config.floatingPoints) {
+            let x = 10 ** this.config.floatingPoints;
+            return Math.round(parseFloat(n) * x) / x;
+        } else {
+            return n;
+        }
+    },
 
-	// Define start sequence.
-	start: function () {
-		Log.info('Starting module: ' + this.name);
+    // Define start sequence.
+    start: function () {
+        Log.info("Starting module: " + this.name);
 
-		this.domains_being_blocked = null;
-		this.dns_queries_today = null;
-		this.ads_blocked_today = null;
-		this.ads_percentage_today = null;
-		this.top_sources = null;
+        this.domains_being_blocked = null;
+        this.dns_queries_today = null;
+        this.ads_blocked_today = null;
+        this.ads_percentage_today = null;
+        this.top_sources = null;
 
-		this.loaded = false;
-		this.scheduleUpdate(this.config.initialLoadDelay);
-	},
+        this.loaded = false;
+        this.scheduleUpdate(this.config.initialLoadDelay);
+    },
 
-	// Override dom generator.
-	getDom: function () {
-		let wrapper = document.createElement('div');
+    // Override dom generator.
+    getDom: function () {
+        let wrapper = document.createElement("div");
 
-		if (!this.loaded) {
-			wrapper.innerHTML = this.translate('LOADING...');
-			wrapper.className = 'dimmed light';
-			return wrapper;
-		}
+        if (!this.loaded) {
+            wrapper.innerHTML = this.translate("LOADING...");
+            wrapper.className = "dimmed light";
+            return wrapper;
+        }
 
-		let header = document.createElement('div');
-		header.className = 'small bright';
-		header.innerHTML = this.formatInt(this.ads_blocked_today) + ' ads blocked today. (' + this.formatFloat(this.ads_percentage_today) + '%)';
-		wrapper.appendChild(header);
+        let header = document.createElement("div");
+        header.className = "small bright";
+        header.innerHTML =
+            this.formatInt(this.ads_blocked_today) +
+            " ads blocked today. (" +
+            this.formatFloat(this.ads_percentage_today) +
+            "%)";
+        wrapper.appendChild(header);
 
-		if (this.top_sources && Object.keys(this.top_sources).length) {
-			let table = document.createElement('table');
-			table.className = 'xsmall light';
-			wrapper.appendChild(table);
+        if (this.top_sources && Object.keys(this.top_sources).length) {
+            let table = document.createElement("table");
+            table.className = "xsmall light";
+            wrapper.appendChild(table);
 
-			let thead = document.createElement('thead');
-			table.appendChild(thead);
+            let thead = document.createElement("thead");
+            table.appendChild(thead);
 
-			let row = document.createElement('tr');
-			thead.appendChild(row);
+            let row = document.createElement("tr");
+            thead.appendChild(row);
 
-			let sourceCell = document.createElement('th');
-			sourceCell.innerHTML = 'Client';
-			row.appendChild(sourceCell);
+            let sourceCell = document.createElement("th");
+            sourceCell.innerHTML = "Client";
+            row.appendChild(sourceCell);
 
-			let countCell = document.createElement('th');
-			countCell.innerHTML = 'Requests';
-			row.appendChild(countCell);
+            let countCell = document.createElement("th");
+            countCell.innerHTML = "Requests";
+            row.appendChild(countCell);
 
-			let tbody = document.createElement('tbody');
-			table.appendChild(tbody);
+            let tbody = document.createElement("tbody");
+            table.appendChild(tbody);
 
-			for (let source in this.top_sources) {
-				let adCount = this.top_sources[source];
+            for (let source in this.top_sources) {
+                let adCount = this.top_sources[source];
 
-				if (this.config.showSourceHostnameOnly) {
-					source = source.split("|")[0];
-				}
+                if (this.config.showSourceHostnameOnly) {
+                    source = source.split("|")[0];
+                }
 
-				let row = document.createElement('tr');
-				tbody.appendChild(row);
+                let row = document.createElement("tr");
+                tbody.appendChild(row);
 
-				let sourceCell = document.createElement('td');
-				sourceCell.innerHTML = source;
-				row.appendChild(sourceCell);
+                let sourceCell = document.createElement("td");
+                sourceCell.innerHTML = source;
+                row.appendChild(sourceCell);
 
-				let countCell = document.createElement('td');
-				countCell.innerHTML = this.formatInt(adCount);
-				row.appendChild(countCell);
-			}
-		}
+                let countCell = document.createElement("td");
+                countCell.innerHTML = this.formatInt(adCount);
+                row.appendChild(countCell);
+            }
+        }
 
-		let footer = document.createElement('div')
-		footer.className = 'xsmall';
-		footer.innerHTML = this.formatInt(this.dns_queries_today) + ' DNS queries, ' + this.formatInt(this.domains_being_blocked) + ' domains blacklisted.'
-		wrapper.appendChild(footer);
+        let footer = document.createElement("div");
+        footer.className = "xsmall";
+        footer.innerHTML =
+            this.formatInt(this.dns_queries_today) +
+            " DNS queries, " +
+            this.formatInt(this.domains_being_blocked) +
+            " domains blacklisted.";
+        wrapper.appendChild(footer);
 
-		return wrapper;
-	},
+        return wrapper;
+    },
 
-	updateStats: function () {
-		Log.info(this.name + ": Getting data");
+    updateStats: function () {
+        Log.info(this.name + ": Getting data");
 
-		this.sendSocketNotification("GET_PIHOLE", {
-			config: this.config
-		});
-	},
+        this.sendSocketNotification("GET_PIHOLE", {
+            config: this.config,
+        });
+    },
 
-	// Handle node helper response
-	socketNotificationReceived: function (notification, payload) {
-		if (notification === "PIHOLE_DATA") {
-			this.processSummary(payload);
-			this.loaded = true;
-		}
-		else if (notification === "PIHOLE_SOURCES") {
-			this.processSources(payload);
-		}
+    // Handle node helper response
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "PIHOLE_DATA") {
+            this.processSummary(payload);
+            this.loaded = true;
+        } else if (notification === "PIHOLE_SOURCES") {
+            this.processSources(payload);
+        }
 
-		this.updateDom(this.config.animationSpeed);
-	},
+        this.updateDom(this.config.animationSpeed);
+    },
 
-	scheduleUpdate: function (delay) {
-		let nextLoad = this.config.updateInterval;
-		if (typeof delay !== 'undefined' && delay >= 0) {
-			nextLoad = delay;
-		}
+    scheduleUpdate: function (delay) {
+        let nextLoad = this.config.updateInterval;
+        if (typeof delay !== "undefined" && delay >= 0) {
+            nextLoad = delay;
+        }
 
-		let self = this;
-		setTimeout(function () {
-			self.updateStats();
-			self.scheduleUpdate(self.config.updateInterval);
-		}, nextLoad);
-	},
+        let self = this;
+        setTimeout(function () {
+            self.updateStats();
+            self.scheduleUpdate(self.config.updateInterval);
+        }, nextLoad);
+    },
 
-	processSummary: function (data) {
-		if (!data) {
-			// Did not receive usable new data.
-			return;
-		}
+    processSummary: function (data) {
+        if (!data) {
+            // Did not receive usable new data.
+            return;
+        }
 
-		this.domains_being_blocked = data['domains_being_blocked'] || '0';
-		this.dns_queries_today = data['dns_queries_today'] || '0';
-		this.ads_blocked_today = data['ads_blocked_today'] || '0';
-		this.ads_percentage_today = data['ads_percentage_today'] || '0.0';
-	},
+        this.domains_being_blocked = data["domains_being_blocked"] || "0";
+        this.dns_queries_today = data["dns_queries_today"] || "0";
+        this.ads_blocked_today = data["ads_blocked_today"] || "0";
+        this.ads_percentage_today = data["ads_percentage_today"] || "0.0";
+    },
 
-	processSources: function (data) {
-		if (!data) {
-			// Did not receive usable new data.
-			return;
-		}
+    processSources: function (data) {
+        if (!data) {
+            // Did not receive usable new data.
+            return;
+        }
 
-		this.top_sources = data['top_sources'] || [];
-	},
-
+        this.top_sources = data["top_sources"] || [];
+    },
 });
--- node_helper.js
+++ node_helper.js
@@ -3,71 +3,80 @@ const fetch = require("node-fetch");
 const NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
+    start: function () {
+        Log.info("Starting node_helper for module [" + this.name + "]");
+    },
 
-	start: function () {
-		Log.info("Starting node_helper for module [" + this.name + "]");
-	},
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "GET_PIHOLE") {
+            let config = payload.config;
 
-	socketNotificationReceived: function (notification, payload) {
-		if (notification === "GET_PIHOLE") {
-			let config = payload.config;
+            if (!this.isValidURL(config.apiURL)) {
+                Log.error(this.name + ": The apiURL is not a valid URL");
+                return;
+            }
 
-			if (!this.isValidURL(config.apiURL)) {
-				Log.error(this.name + ": The apiURL is not a valid URL");
-				return
-			}
+            Log.info("Notification: " + notification + " Payload: " + payload);
+            this.getPiholeData(config, { summary: 1 }, "PIHOLE_DATA");
 
-			Log.info("Notification: " + notification + " Payload: " + payload);
-			this.getPiholeData(config, {summary: 1}, 'PIHOLE_DATA');
+            if (config.showSources && config.sourcesCount > 0) {
+                if (config.showSources && !config.apiToken) {
+                    Log.error(
+                        this.name +
+                            ": Can't load sources because the apiKey is not set.",
+                    );
+                } else {
+                    this.getPiholeData(
+                        config,
+                        { getQuerySources: config.sourcesCount },
+                        "PIHOLE_SOURCES",
+                    );
+                }
+            }
+        }
+    },
 
-			if (config.showSources && config.sourcesCount > 0) {
-				if (config.showSources && !config.apiToken) {
-					Log.error(this.name + ": Can't load sources because the apiKey is not set." );
-				} else {
-					this.getPiholeData(config, {getQuerySources: config.sourcesCount}, 'PIHOLE_SOURCES');
-				}
-			}
-		}
-	},
+    isValidURL: function (url) {
+        try {
+            new URL(url);
+            return true;
+        } catch (_) {
+            return false;
+        }
+    },
 
-	isValidURL: function (url) {
-		try {
-			new URL(url);
-			return true
-		} catch (_) {
-			return false;
-		}
-	},
+    buildURL: function (config, params) {
+        params = params || {};
 
-	buildURL: function (config, params) {
-		params = params || {}
+        if (config.apiToken && !params.hasOwnProperty("auth")) {
+            params["auth"] = config.apiToken;
+        }
 
-		if (config.apiToken && !params.hasOwnProperty('auth')) {
-			params['auth'] = config.apiToken
-		}
+        const url = new URL(config.apiURL);
 
-		const url = new URL(config.apiURL);
+        if (config.port) {
+            url.port = config.port;
+        }
 
-		if (config.port) {
-			url.port = config.port;
-		}
+        url.search = new URLSearchParams(params).toString();
+        return url.toString();
+    },
 
-		url.search = new URLSearchParams(params).toString();
-		return url.toString();
-	},
+    getPiholeData: function (config, params, notification) {
+        const self = this,
+            url = self.buildURL(config, params),
+            headers = { Referer: url };
 
-	getPiholeData: function (config, params, notification) {
-		const self = this,
-			url = self.buildURL(config, params),
-			headers = {'Referer': url};
-
-		this.sendSocketNotification('LOADING_PIHOLE_URL', url)
-		fetch(url, { headers: headers}).then(response => {
-			response.json().then(data => {
-				self.sendSocketNotification(notification, data);
-			})
-		}, error => {
-			Log.error(self.name + ' ERROR:', error);
-		})
-	}
+        this.sendSocketNotification("LOADING_PIHOLE_URL", url);
+        fetch(url, { headers: headers }).then(
+            (response) => {
+                response.json().then((data) => {
+                    self.sendSocketNotification(notification, data);
+                });
+            },
+            (error) => {
+                Log.error(self.name + " ERROR:", error);
+            },
+        );
+    },
 });
